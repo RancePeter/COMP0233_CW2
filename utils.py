0b58c9a5ba7c65b2834edd1c9e61e73a3d18e5ad
Tianxu Yang <rmaptya@ucl.ac.uk>
2022-11-14 19:43:41 +0000

    fulfill the filepaht and collection

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 from cities import City, CityCollection
+import csv
+from pathlib import Path
+file_path = Path("attendee_location.csv")
 
 def read_attendees_file(filepath: Path) -> CityCollection:
-    raise NotImplementedError
+    csv_data = csv.reader(open(filepath))
+    headers = next(csv_data)  #to read the first row
+    
