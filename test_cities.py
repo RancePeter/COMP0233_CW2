9c5060a9165333f95bb272887e973ff99237d313
Tianxu Yang <rmaptya@ucl.ac.uk>
2022-11-15 18:55:26 +0000

    to text the code and type the frame

---
+++ test_cities.py
@@ -0,0 +1,25 @@
+from cities import City, CityCollection
+
+
+def main():
+    # fixtures and test parametrisation
+    test_data_valid()
+    test_work_transportation()
+    test_work_collection()
+    test_work_host()
+    test_work_sorted()
+
+
+if __name__ == '__main__':
+    main()
+
+
+def test_data_valid():
+
+def test_work_transportation():
+
+def test_work_collection():
+
+def test_work_host():
+
+def test_work_sorted():
